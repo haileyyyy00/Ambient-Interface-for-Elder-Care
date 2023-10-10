b97f8e1d7f23e7f7cd9dad0a918ed53d62d0cb6c
Kushagra Agrawal <agrawalkushagra647@gmail.com>
2023-10-10 14:15:56 +1000

    Added code for heating element

---
+++ deco/deco.ino
@@ -0,0 +1,25 @@
+// the setup routine runs once when you press reset:
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(2,OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  
+  float voltage = sensorValue * (5.0 / 1023.0);
+  float temperature = voltage * 100;
+  Serial.println(temperature);
+  Serial.flush();
+  startHeat(30,temperature);
+  delay(1000);
+  
+}
+
+void startHeat(int threshold, int temp){
+  int fanSpeed = 255 - (abs(threshold-temp)*10);
+  analogWrite( 9, fanSpeed );
+  }
